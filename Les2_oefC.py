f45542b486d718251fa52af65c7e7e9b4d7057a2
charlotte <charlotte.jilderda@gmail.com>
2026-05-29 12:15:28 +0200

    29052026

---
+++ Les2_oefC.py
@@ -0,0 +1,11 @@
+a = 8
+b = 4
+
+som = a + b 
+verschil = a - b
+product = a * b 
+deling = a / b
+print(f"som: {som}")
+print(f"verschil: {verschil}")
+print(f"product: {product}")
+print(f"deling: {deling}")
