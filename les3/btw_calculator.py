e50f18755ee01b7c4ba3d54a4d6e54d04e5c12a0
charlotte <charlotte.jilderda@gmail.com>
2026-06-05 10:05:50 +0200

    202606051005

--- les3/btw_calculator.py
+++ les3/btw_calculator.py
@@ -1,3 +1,1 @@
-print("Welkom bij de BTW-calculator van primabouw BV")
-naam = input("Wat is jou naam?")
-print("Hallo" + naam + "! Ik ga je helpen met BTW-berekeningen")
+# btw_calculator.py - gemaakt door Charlotte Jilderda
