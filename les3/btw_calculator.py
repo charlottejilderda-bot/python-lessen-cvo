6f5e4d64fcfbe8592f97a46a1ce4da3534d7e47e
charlotte <charlotte.jilderda@gmail.com>
2026-06-05 10:25:27 +0200

    202606051025

--- les3/btw_calculator.py
+++ les3/btw_calculator.py
@@ -1,1 +1,15 @@
 # btw_calculator.py - gemaakt door Charlotte Jilderda
+
+print("=" * 45)
+print(" BTW-Calculator -PrimaBouw BV")
+print("=" * 45)
+gebruiker = input("Jouw naam:")
+print("Welkom, " + gebruiker + "!")
+
+bedrag_str = input("Voer het bedrag in (excl. BTW, in €):")
+bedrag = float(bedrag_str)
+tarief_str = input("Welk BTW-tarief?(6, 12 of 21):")
+tarief = int(tarief_str)
+
+btw_bedrag = bedrag * (tarief/100)
+totaal = bedrag + btw_bedrag
