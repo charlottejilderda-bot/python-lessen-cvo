bea9ae9e3ae8f4e025b61b2a476f958da4214033
charlotte <charlotte.jilderda@gmail.com>
2026-06-05 12:20:58 +0200

    202606051220

---
+++ les3/temp_calculator.py
@@ -0,0 +1,11 @@
+# temp_calculator.py - gemaakt door Charlotte 
+
+print("=" * 40)
+print(" Temperatuuromrekening F -> C")
+print("=" * 40)
+
+Farenheit = float(input("Voer temperatuur in (°F):"))
+Celsius = (Farenheit - 32) / 1.8
+print(f"{Farenheit}°F = {Celsius:.1f}°C")
+
+
