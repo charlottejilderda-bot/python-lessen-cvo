5fccb19bd1cd5c01635759d72ba4b7298d2f92ba
charlotte <charlotte.jilderda@gmail.com>
2026-06-12 11:39:29 +0200

    202606121139

---
+++ les4/geintergreed_menu.py
@@ -0,0 +1,26 @@
+#geintegreed_menu.py - gemaakt door Charlotte
+
+while True: 
+
+    print("\n"+"=" * 45)
+    print("HOOFDMENU ERPM-SYSTEEM PRIMABOUW")
+    print("=" * 45)
+    print("1)BTW_Calculator(Les 3)")
+    print("2)BTW_Calculator(Les 4)")
+    print("3)BTW_Calculator(Les 4)")
+    print("4)Applicatie afsluiten")
+    print("=" * 45)
+
+    Keuze = input("Maak uw keuze(1-4):")
+    if keuze == "1":
+        print("\n---[OPSTARTEN BTW_CALCULATOR]---")
+        #Schrijf hier de BTW-berekening (invoer bedrag, invoer tarief, berekening en f-string print)
+    elif keuze == "2":
+        print("\n---[OPSTARTEN KORTINGS_CALCULATOR]---")
+    elif keuze == "3":
+        print("\n---[OPSTARTEN WERF-VEILIGHEID]---")
+    elif keuze == "4":
+        print("\nBedankt voor het gebruiken van het PrimaBouw systeem. Tot ziens!")
+        break #Dit breekt de 'while True' lus en stopt het programma 
+    else:
+        print("ongeldige keuze! Voer een cijfer van 1 tot en met 4 in.")
