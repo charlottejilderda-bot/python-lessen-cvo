a9a75cb7ec6b1343bfed81545fa6e7e56a1073c1
charlotte <charlotte.jilderda@gmail.com>
2026-06-12 10:05:43 +0200

    202606121005

--- les4/korting_calculator.py
+++ les4/korting_calculator.py
@@ -5,7 +5,21 @@ print("Korting Checker - PrimabouwBV")
 print("=" * 45)
 totaalbedrag = float(input("Voer het totale orderbedrag in (€):"))
 
-if totaalbedrag > 1000: 
-    print("Hoera! Deze klant heeft recht op volumekorting.")
+if totaalbedrag >= 2500: 
+    korting_pct = 15
+elif totaalbedrag >= 1000:
+    korting_pct = 10
+elif totaalbedrag >= 500:
+    korting_pct = 5
 else:
-    print("Status: Standaard tarief van toepassing (geen korting).")
+    korting_pct = 0
+
+kortingsbedrag = totaalbedrag * (korting_pct/100)
+eindbedrag = totaalbedrag - kortingsbedrag
+
+print("-" * 45)
+print(f"Bruto orderbedrag:€{totaalbedrag:.2f}")
+print(f"Kortingsprencentage:{korting_pct}%")
+print(f"Kortingsbedrag:€ {kortingsbedrag:.2f}")
+print(f"Netto te betalen:€ {eindbedrag:.2f}")
+print("-" * 45)
